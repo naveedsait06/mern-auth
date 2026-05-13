ca73f5ecee5cd2b32dbb921fba930e9f40773514
Naveed Sait <naveedsait06@gmail.com>
2026-05-14 00:56:09 +0530

    nodemailer

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,16 +1,18 @@
 import nodemailer from 'nodemailer';
+import dns from 'dns'; // Add this import
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-brevo.com',
     port: 587,
-    secure: false, // Use false for 587
+    secure: false,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
-    // Adding these two lines helps us debug in the Render Logs
-    logger: true,
-    debug: true 
+    // Force IPv4 using the family: 4 setting
+    dnsLookup: (hostname, options, callback) => {
+        dns.lookup(hostname, { family: 4 }, callback);
+    }
 });
 
 export default transporter;
