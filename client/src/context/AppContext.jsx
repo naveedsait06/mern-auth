f3f9f0b5f32a4d136540ee28bf0849b0fc4c7cf0
Naveed Sait <naveedsait06@gmail.com>
2026-05-13 21:57:18 +0530

    fix: remove brackets from backendUrl string

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContextProvider = (props) => {
 
     axios.defaults.withCredentials = true;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+   const backendUrl = "https://mern-auth-21ur.onrender.com"
     const [isLoggedin, setIsLoggedin] = useState(false);
     const [userData, setUserData] = useState(null);
 
